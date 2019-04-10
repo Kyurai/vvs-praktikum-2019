36f0b71ba1ab1fca29543cf430a4c9f5d5952595
Dustin Hellmann <dustinhellmann@t-online.de>
2019-04-10 19:40:53 +0200

    Added stylesheets

--- persist-chat/pchat.py
+++ persist-chat/pchat.py
@@ -4,9 +4,6 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-# global variables
-serverStartupTime = datetime.datetime.now()
-
 
 class ChatMessage(db.Model):
     user = db.StringProperty(required=True)
@@ -17,33 +14,39 @@ class ChatMessage(db.Model):
         return "%s (%s): %s" % (self.user, self.timestamp, self.message)
 
 
+startTimestamp = datetime.datetime.now()
+
+
 class ChatRoomPage(webapp.RequestHandler):
     def get(self):
         self.response.headers["Content-Type"] = "text/html"
         self.response.out.write("""
             <html>
                 <head>
-                    <title>Dustins's AppEngine Chat Room</title>
+                    <title>Dustin's AppEngine Chat Room</title>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
                 </head>
                 <body>
-                    <h1>Welcome to Dustins's AppEngine Chat Room</h1>
+                    <h1>Welcome to Dustin's AppEngine Chat Room</h1>
                     <p>(Current time is %s)</p>
                     <p>(Server started at %s)</p>
-            """ % (datetime.datetime.now(), serverStartupTime))
+                    """ % (datetime.datetime.now(), startTimestamp))
         # Output the set of chat messages
         messages = db.GqlQuery("SELECT * From ChatMessage ORDER BY timestamp")
         for msg in messages:
             self.response.out.write("<p>%s</p>" % msg)
         self.response.out.write("""
-            <form action="" method="post">
-            <div><b>Name:</b>
-            <textarea name="name" rows="1" cols="20"></textarea></div>
-            <p><b>Message</b></p>
-            <div><textarea name="message" rows="5" cols="60"></textarea></div>
-            <div><input type="submit" value="Send ChatMessage"></input></div>
-            </form>
-          </body>
-        </html>
+                    <form action="/talk" method="post">
+                        <div>
+                            <b>Name:</b>
+                            <textarea name="name" rows="1" cols="20"></textarea>
+                        </div>
+                        <p><b>Message</b></p>
+                        <div><textarea name="message" rows="5" cols="60"></textarea></div>
+                        <div><input type="submit" value="Send ChatMessage"></input></div>
+                    </form>
+                </body>
+            </html>
         """)
 
 
@@ -53,28 +56,30 @@ class ChatRoomCountViewPage(webapp.RequestHandler):
         self.response.out.write("""
             <html>
                 <head>
-                    <title>Dustins's AppEngine Chat Room (last 20)</title>
+                    <title>Dustin's AppEngine Chat Room</title>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
                 </head>
-            <body>
-                <h1>Welcome to Dustins's AppEngine Chat Room</h1>
-                <p>(Current time is %s; viewing the last 20 messages.)</p>
-            """ % (datetime.datetime.now()))
+                <body>
+                    <h1>Welcome to Dustin's AppEngine Chat Room</h1>
+                    <p>(Current time is %s; viewing the 20 last messages)</p>
+                    <p>(Server started at %s)</p>
+                    """ % (datetime.datetime.now(), startTimestamp))
         # Output the set of chat messages
-        messages = db.GqlQuery("SELECT * From ChatMessage " +
-                               "ORDER BY timestamp DESC LIMIT 20").fetch(20)
-        messages.reverse()
-        for msg in list(messages):
+        messages = db.GqlQuery("SELECT * From ChatMessage ORDER BY timestamp DESC LIMIT 20").fetch(20)
+        for msg in messages:
             self.response.out.write("<p>%s</p>" % msg)
         self.response.out.write("""
-            <form action="/talk" method="post">
-          <div><b>Name:</b>
-            <textarea name="name" rows="1" cols="20"></textarea></div>
-            <p><b>Message</b></p>
-            <div><textarea name="message" rows="5" cols="60"></textarea></div>
-            <div><input type="submit" value="Send ChatMessage"/></div>
-            </form>
-          </body>
-        </html>
+                    <form action="/talk" method="post">
+                        <div>
+                            <b>Name:</b>
+                            <textarea name="name" rows="1" cols="20"></textarea>
+                        </div>
+                        <p><b>Message</b></p>
+                        <div><textarea name="message" rows="5" cols="60"></textarea></div>
+                        <div><input type="submit" value="Send ChatMessage"></input></div>
+                    </form>
+                </body>
+            </html>
         """)
 
 
@@ -84,39 +89,40 @@ class ChatRoomTimeViewPage(webapp.RequestHandler):
         self.response.out.write("""
             <html>
                 <head>
-                    <title>Dustins's AppEngine Chat Room</title>
+                    <title>Dustin's AppEngine Chat Room</title>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
                 </head>
                 <body>
-                    <h1>Welcome to Dustins's AppEngine Chat Room</h1>
+                    <h1>Welcome to Dustin's AppEngine Chat Room</h1>
                     <p>(Current time is %s)</p>
                     <p>(Server started at %s)</p>
-            """ % (datetime.datetime.now(), serverStartupTime))
+                    """ % (datetime.datetime.now(), startTimestamp))
         # Output the set of chat messages
         messages = ChatMessage.gql("WHERE timestamp > :fiveago ORDER BY timestamp",
                                    fiveago=datetime.datetime.now() - datetime.timedelta(minutes=5))
         for msg in messages:
             self.response.out.write("<p>%s</p>" % msg)
         self.response.out.write("""
-            <form action="" method="post">
-            <div><b>Name:</b>
-            <textarea name="name" rows="1" cols="20"></textarea></div>
-            <p><b>Message</b></p>
-            <div><textarea name="message" rows="5" cols="60"></textarea></div>
-            <div><input type="submit" value="Send ChatMessage"></input></div>
-            </form>
-          </body>
-        </html>
+                    <form action="/talk" method="post">
+                        <div>
+                            <b>Name:</b>
+                            <textarea name="name" rows="1" cols="20"></textarea>
+                        </div>
+                        <p><b>Message</b></p>
+                        <div><textarea name="message" rows="5" cols="60"></textarea></div>
+                        <div><input type="submit" value="Send ChatMessage"></input></div>
+                    </form>
+                </body>
+            </html>
         """)
 
 
 class ChatRoomPoster(webapp.RequestHandler):
     def post(self):
         chatter = self.request.get("name")
-        msgtext = self.request.get("message")
-        msg = ChatMessage(user=chatter, message=msgtext)
+        msgtxt = self.request.get("message")
+        msg = ChatMessage(user=chatter, message=msgtxt)
         msg.put()
-        # Now that we've added the message to the chat, we'll redirect
-        # to the root page,
         self.redirect('/limited/time')
 
 
